e67d4870d299f146995ea69d978af4e1275fd74a
Wayne Newa <newawayne@gmail.com>
2025-11-25 19:20:12 +0300

    Fix Render deploy: publish folder & rewrite rule

--- agritrack-frontend/src/api/api.js
+++ agritrack-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://agritrack-system-4.onrender.com/',
 });
 
 api.interceptors.request.use(config => {
